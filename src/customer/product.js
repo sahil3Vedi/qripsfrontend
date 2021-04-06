1398e14ea9c81eb4f49013bf0848c5dc17485afa
sahil3vedi <sahiltrivediw@gmail.com>
2021-04-06 22:34:14 +0530

    added item display for products

--- src/customer/product.js
+++ src/customer/product.js
@@ -1,22 +1,42 @@
 import React, {Component} from 'react'
 import Spinner from '../customercomps/spinner'
+import Item from '../customercomps/item'
+import axios from 'axios'
 
 class Product extends Component{
+
     constructor(props){
         super(props)
         this.state={
             pathname : props.pathname,
-            loading: true
+            loading: true,
+            products: [],
         }
     }
+
+    componentDidMount(){
+        console.log(this.state.pathname.split('/')[2])
+        let params = {params:{"category": "cheese"}}
+        axios.get(`${process.env.REACT_APP_BACKEND}/products/fetch/${this.state.pathname.split('/')[2]}`)
+        .then(res=>{
+            this.setState({
+                products: res.data,
+                loading: false
+            })
+        })
+    }
+
     render(){
+        let items = this.state.products.map(p=><Item data={p} key={p.supplier_name}/>)
         return (
             <div>
                 {
                     this.state.loading ?
                     <Spinner tip="Loading..."/>
                     :
-                    <p>{this.state.pathname}</p>
+                    <div style={{display:"grid",gridTemplateColumns:"1fr 1fr",columnGap:"10px",margin:"10px 20px"}}>
+                        {items}
+                    </div>
                 }
             </div>
         )
---
+++ src/customercomps/item.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+class Item extends Component{
+    render(){
+        return(
+            <p>Item</p>
+        )
+    }
+}
+
+export default Item
