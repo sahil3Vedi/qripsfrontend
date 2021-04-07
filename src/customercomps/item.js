9e1c10f17bbb05c7cbf616313ffc289c2adf36f9
sahil3vedi <sahiltrivediw@gmail.com>
2021-04-07 18:09:31 +0530

    awda

--- src/customercomps/item.js
+++ src/customercomps/item.js
@@ -17,15 +17,15 @@ class Item extends Component{
                     <div className="div-product-image-overlay"><img className="product-image" src={data.supplier_images[0]}/></div>
                 </div>
                 <div style={{backgroundColor:`${data.color}`}} className="product">
-                    <p>{data.shop_company}</p>
-                    <p>{data.shop_name}</p>
+                    <p className="item-subtitle">{data.shop_company}</p>
+                    <p className="item-title">{data.shop_name}</p>
                     <div className="pricing-qty">
-                        <div><p>&#8377;{data.shop_price}/-</p></div>
+                        <div><p className="item-subtitle">&#8377;{data.shop_price}/-</p></div>
                         <div></div>
-                        <div><p>300g</p></div>
+                        <div><p className="item-subtitle">300g</p></div>
                     </div>
                     <div className="div-add-to-cart">
-                        <Button>Add to Cart</Button>
+                        <Button className="btn-add-to-cart">Add to Cart</Button>
                     </div>
                 </div>
             </div>
